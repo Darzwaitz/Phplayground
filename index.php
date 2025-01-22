1b6841702c9e22805d60d0af7cdda177e242a956
darzwaitz <darzwaitz@yahoo.com>
2025-01-22 23:54:44 +0000

    setting up database connection using PDO - part 1

--- index.php
+++ index.php
@@ -3,3 +3,10 @@
 require 'functions.php';
 
 require 'router.php';
+
+
+// connect to sql db using PDO
+
+$dsn = "mysql:host=localhost;port=3000;dbname=phplayground;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
