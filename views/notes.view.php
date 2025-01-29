ef7248d0902ab24235e67b38d6fe666394126b84
darzwaitz <darzwaitz@yahoo.com>
2025-01-29 23:55:22 +0000

    adding another page and its controller - Part 2

---
+++ views/notes.view.php
@@ -0,0 +1,12 @@
+<?php require('partials/head.php') ?>
+
+<?php require('partials/nav.php') ?>
+
+<?php require('partials/header.php') ?>
+
+
+<main>
+  <h1>Notes page</h1>
+</main>
+
+<?php require('partials/footer.php') ?>
