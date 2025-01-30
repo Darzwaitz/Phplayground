e3f2acfe709cef192602a8f17afe35c024076558
darzwaitz <darzwaitz@yahoo.com>
2025-01-30 23:43:32 +0000

    adding content from sql query in notes page

--- controllers/notes.php
+++ controllers/notes.php
@@ -1,8 +1,11 @@
 <?php
 
+$config = require('config.php');
 
+$db = new Database($config['database']);
 
 $heading = 'Notes';
 
+// dd($db);
 
 require 'views/notes.view.php';
--- index.php
+++ index.php
@@ -2,27 +2,24 @@
 
 require 'functions.php';
 
-require 'router.php';
-
 require 'Database.php';
 
-$config = require('config.php');
+require 'router.php';
 
-$db = new Database($config['database']);
 
-if ($_GET) {
+// if ($_GET) {
 
-    $id = $_GET['id'];
-    $query = "SELECT * FROM `posts` where id = :id";
+//     $id = $_GET['id'];
+//     $query = "SELECT * FROM `posts` where id = :id";
 
-    // :: is scope resolution operator
-    $posts = $db->query($query, [':id' => $id])->fetchAll();
+//     // :: is scope resolution operator
+//     $posts = $db->query($query, [':id' => $id])->fetchAll();
 
-    // dd($posts);
+//     // dd($posts);
 
-    foreach ($posts as $post) {
-        echo "<li>" . $post['title'] . "</li>";
-    }
-} else {
-    echo "<li>" . 'No posts available from query' . "</li>";
-}
+//     foreach ($posts as $post) {
+//         echo "<li>" . $post['title'] . "</li>";
+//     }
+// } else {
+//     echo "<li>" . 'No posts available from query' . "</li>";
+// }
