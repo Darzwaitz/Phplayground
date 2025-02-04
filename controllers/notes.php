2fa5cf90f365f7f4cc78bd592c22c4753b5af0ea
darzwaitz <darzwaitz@yahoo.com>
2025-02-04 23:58:14 +0000

    updating notes page

--- controllers/notes.php
+++ controllers/notes.php
@@ -6,6 +6,8 @@ $db = new Database($config['database']);
 
 $heading = 'Notes';
 
+$notes = $db->query('select * from notes where user_id = 1')->fetchAll();
+
 // dd($db);
 
 require 'views/notes.view.php';
