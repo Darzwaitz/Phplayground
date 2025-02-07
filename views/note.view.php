04303a8bd78659897ce9d10103edb7218cd5f5b2
darzwaitz <darzwaitz@yahoo.com>
2025-02-07 00:58:43 +0000

    adding auth by checking id from database

--- views/note.view.php
+++ views/note.view.php
@@ -6,10 +6,10 @@
 
 
 <main>
-  <h1>Notes page</h1>
+  <h1>Note page</h1>
   <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
     <p class="mb-6">
-      <a href="/notes" class="text-blue-500 underline">Go home...</a>
+      <a href="/notes" class="text-blue-500 underline">Go back...</a>
     </p>
 
     <p>
