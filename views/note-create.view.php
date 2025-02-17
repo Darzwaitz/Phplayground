9dbc56898b981bb328ec1043a5e3a1594321cb1f
darzwaitz <darzwaitz@yahoo.com>
2025-02-17 23:57:10 +0000

    adding functionality to create a note - Part 2

---
+++ views/note-create.view.php
@@ -0,0 +1,16 @@
+<?php require('partials/head.php') ?>
+
+<?php require('partials/nav.php') ?>
+
+<?php require('partials/header.php') ?>
+
+
+
+<main>
+  <h1>Notes page</h1>
+  <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+    <h1>Create a Note</h1>
+  </div>
+</main>
+
+<?php require('partials/footer.php') ?>
