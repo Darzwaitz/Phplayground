3060bc5c641e8ab56431d9125306e33cc8833812
darzwaitz <darzwaitz@yahoo.com>
2025-02-25 23:52:57 +0000

    refactoring create note form structure and styling

--- src/views/partials/head.php
+++ src/views/partials/head.php
@@ -7,9 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- npx @tailwindcss/cli -i src/css/main.css -o src/css/output.css --watch
  -->
-    <link rel="stylesheet" href="src/css/output.css">
+    <!-- <link rel="stylesheet" href="src/css/output.css"> -->
 
-    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Php doc</title>
 </head>
 
