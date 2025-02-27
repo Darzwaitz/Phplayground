8ceb84432a15bfd320764e8af1da57dc148c2426
darzwaitz <darzwaitz@yahoo.com>
2025-02-27 23:55:29 +0000

    form validation - part 1

--- src/controllers/note-create.php
+++ src/controllers/note-create.php
@@ -8,10 +8,18 @@ $heading = 'Create note';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // dd($_POST);
-    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
-        'body' => $_POST['body'],
-        'user_id' => 1,
-    ]);
+    $errors = [];
+
+    if (strlen($_POST['body']) === 0) {
+        $errors['body'] = 'A body is required';
+    }
+
+    if (empty($errors)) {
+        $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => 1,
+        ]);
+    }
 };
 
 require 'src/views/note-create.view.php';
