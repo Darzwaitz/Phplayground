4124e46e1cd3728bd98dcf9a248537127e2d98b8
darzwaitz <darzwaitz@yahoo.com>
2025-03-05 23:03:39 +0000

    moved initial first project to single folder - in initial folder first project; updated tailwind config to tailwind v4

--- php site from scratch/src/views/partials/head.php
+++ php site from scratch/src/views/partials/head.php
@@ -5,9 +5,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- npx @tailwindcss/cli -i src/css/main.css -o src/css/output.css --watch
- -->
-    <!-- <link rel="stylesheet" href="src/css/output.css"> -->
+
+    <link rel="stylesheet" href="src/css/output.css">
+    <!-- <link href="../../css/output.css" rel="stylesheet"> -->
 
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Php doc</title>
